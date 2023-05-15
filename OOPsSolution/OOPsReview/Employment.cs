2608be319560edd2f9793221409f02a1d1e5d1ba
Ekamjot Kaur <e3@nait.ca>
2023-05-14 22:54:23 -0600

    creating first class library, enum class,  employment class

---
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -0,0 +1,22 @@
+﻿namespace OOPsReview
+{
+    public class Employment
+    {
+        private SupervisoryLevel _Level;
+        private string _Title;
+        private double _years;
+
+        public string Title
+        {
+            get { return _Title; }
+            set {
+                if (string.IsNullOrWhiteSpace(value)) 
+                { 
+                    throw new ArgumentNullException("Title is required");
+                }
+                
+                _Title = value;
+            }
+        }
+    }
+}
---
+++ OOPsSolution/OOPsReview/SupervisoryLevel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public enum SupervisoryLevel
+    {
+       // enum names are strings representing an interger value by default the interger values start at 0 and increment by 1 
+       //one could assign their own values to each of the enum names
+
+        Entry,              //0
+        TeamMwmber,         //1
+        TeamLeader,         //2
+        Supervisor,         //3
+        DepartmentHead,     //4
+        Owner               //5
+    }
+}
