922ea0b4641a307b2869fb9fb915bab6b5a76131
Ekamjot Kaur <e3@nait.ca>
2023-06-28 18:52:18 -0600

    coding of the PageModel

--- WebAppSolution/WebApp/Pages/Samples/StudentMarkReport.cshtml.cs
+++ WebAppSolution/WebApp/Pages/Samples/StudentMarkReport.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.Models; // the namespace of your source code (namespace is WebApp.Models)
-                        // i am telling where to find student model
+                     // i am telling where to find student model
+
 
 
 namespace WebApp.Pages.Samples
@@ -14,9 +16,82 @@ namespace WebApp.Pages.Samples
         // to detrmine if we have data coming in 
         public List<StudentMarks> studentMarks { get; set; } = new List<StudentMarks>();
     
+        // dependency injection (constructor injection technique)
+        // a) create a constructor for your pagemodel class 
+        // b) the services you wish to inject will be parameters on the contructor
+        // c) save the incoming parameter values in a public property.
+
+        public IWebHostEnvironment _webHostEnvironment { get; set; }
+        public StudentMarkReportModel(IWebHostEnvironment env)
+        {
+            _webHostEnvironment = env;
+        }
         public void OnGet()
         {
+            //the student mark report will be loaded as the page comes up for the
+            // first time.
+            //therefore, all code will be in OnGet() event
+
+            //get the path to your web app root
+            string contentPathname = _webHostEnvironment.ContentRootPath;
+            string filePathName = Path.Combine(contentPathname, @"Data\StudentMarks.txt");
+
+            //userdata will contain all of the data file records in an array
+            Array userdata = null;
+
+            //to Parse the csv record into an instance of our class, I have setup
+            // a reusable variable of hold an instance of the class.
+            StudentMarks markRecord = null;
+            try
+            {
+                // There is a file class in PageModel
+                // to use the File class of System.IO you MUST fully qualify your
+                // reference to the class when coding the read method.
+                userdata = System.IO.File.ReadAllLines(filePathName);
+
+                //process each record in the record array
+                // each record could possibly throw an exception while Parsing
+                // you should process all possible records while reporting records
+                // that could not ne parsed
+
+                foreach(string line in userdata)
+                {
+                    try
+                    {
+                        markRecord = StudentMarks.Parse(line);
+                        if(markRecord != null)
+                        {
+                            studentMarks.Add(markRecord);
+                        }
+                    }
+                    catch(FormatException ex)
+                    {
+                        ModelState.AddModelError("Record Format ", $"{GetInnerException(ex).Message}:record{line}");
+                    }
+
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("System Error", $"{GetInnerException(ex).Message}:record{line}");
+                    }
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("File Data Error", $"{GetInnerException(ex).Message}");
+            }
 
         }
-    }
+        public Exception GetInnerException(Exception ex)
+        {
+            while(ex.InnerException !=null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
+        }
+    
+}
 }
